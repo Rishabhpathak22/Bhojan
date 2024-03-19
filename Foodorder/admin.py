bb435ec4546327d6c872829c29241f9cbe79755f
Rishabhpathak22 <Rishabh14rox@gmail.com>
2024-03-19 19:59:06 +0530

    Initial commit

---
+++ Foodorder/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from Foodorder.models import Contact
+from Foodorder.models import Contact2
+from Foodorder.models import Res
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(Contact2)
+admin.site.register(Res)
---
+++ Foodorder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FoodorderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Foodorder'
